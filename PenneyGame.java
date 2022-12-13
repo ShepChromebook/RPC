a7dd250fef8ab516a02f603a938f0b54cbfe55ba
ShepChromebook <93790365+ShepChromebook@users.noreply.github.com>
2022-12-13 01:50:25 +0000

    h

--- PenneyGame.java
+++ PenneyGame.java
@@ -21,35 +21,36 @@ public class PenneyGame {
     private static Scanner input = new Scanner(System.in);
     //
     public static void main(String[] args) {
-        for (int i=0; i<3; i++) {
-            System.out.println("A coin will be flipped over and over.");
-            System.out.println("The goal is to predict a pattern appearing, Ex: 3 heads ina row.");
-            wait(200);
-            System.out.println("Would you like to see an example?");
-            if (chooseExample()) {
-                example();
-            }
-            System.out.println("Okay, how'd you like to play this?\n"
-                        +"  0) Easy: I choose first, randomly\n"
-                        +"  1) Normal: I choose first, strategically\n"
-                        +"  2) Hard: You choose first, I choose strategically");
-            int difficulty = chooseDifficulty();
+        while (true) {
 
-            if (difficulty!=2) {
-                String cpuChoice = cpuChoice(difficulty,"");
-                System.out.println("I'll bet on "+cpuChoice+"\nNow you choose:");
-                String pChoice = pChoice(cpuChoice);
-            } else {
-                System.out.println("Alright, you choose first");
-                String pChoice = pChoice("");
-                String cpuChoice = cpuChoice(difficulty,pChoice);
-                System.out.println("In that case I'll bet on "+cpuChoice);
-            }
-            
-            play(pChoice,cpuChoice);
+        System.out.println("A coin will be flipped over and over.");
+        System.out.println("The goal is to predict a pattern appearing, Ex: 3 heads ina row.");
+        wait(200);
+
+        System.out.println("Would you like to see an example?");
+        if (chooseExample())
+            example();
+
+        System.out.println("Okay, how'd you like to play this?\n"
+                    +"  0) Easy: I choose first, randomly\n"
+                    +"  1) Normal: I choose first, strategically\n"
+                    +"  2) Hard: You choose first, I choose strategically");
+        int difficulty = chooseDifficulty();
+
+        if (difficulty!=2) {
+            String cpuChoice = cpuChoice(difficulty,"");
+            System.out.println("I'll bet on "+cpuChoice+"\nNow you choose:");
+            String pChoice = pChoice(cpuChoice);
+        } else {
+            System.out.println("Alright, you choose first");
+            String pChoice = pChoice("");
+            String cpuChoice = cpuChoice(difficulty,pChoice);
+            System.out.println("In that case I'll bet on "+cpuChoice);
         }
-        System.out.println("Imma stop now");
-        input.close();
+        
+        play(pChoice,cpuChoice);
+        
+        }
     }
     /**
      * @return true if player inputs "yes"/"y", false otherwise
@@ -75,13 +76,14 @@ public class PenneyGame {
         // Input: 0,1,2 or e,n,h or easy,normal,hard
         String INPUT = input.nextLine().toLowerCase();
         switch (INPUT) {
-            case 0: case "e": case "easy":
+            case "0": case "e": case "easy":
                 System.out.println("Guess I'll play like a noob");
                 return 0;
-            case 1: case "n": case "normal":
+            case "1": case "n": case "normal":
+                System.out.println("Aight, I'll go first");
                 return 1;
-            default: case "h": case "hard":
-                System.out.println("Hard it is then")
+            default:
+                System.out.println("Hard it is then");
                 return 2;
         }
     }
@@ -91,32 +93,27 @@ public class PenneyGame {
      * @return cpu's choice, cannot == pChoice
      */
     public static String cpuChoice(int difficulty,String pChoice) {
-        if (difficulty==0) {
+        if (difficulty==0 || difficulty==1) {
             int random8 = (int)(Math.random()*8);
-            switch (random8) {
-                case "HHH";
-                case "HHT";
-                case "HTH";
-                case "HTT";
-                case "THH";
-                case "THT";
-                case "TTH";
-                case "TTT";
+
+            if (difficulty==1) { random8 /= 2; }
+
+            switch (random8) { //"HHH" "HHT" "HTH" "HTT" "THH" "THT" "TTH" "TTT"
+                case 0: return "THH"; //7:1 3:1
+                case 1: return "HHT"; //2:1 2:1
+                case 2: return "TTH"; //2:1 2:1
+                case 3: return "HTT"; //7:1 3:1
+
+                case 4: return "HHH";
+                case 5: return "HTH";
+                case 6: return "THT";
+                case 7: return "TTT";
             }
-        } if (difficulty==1) {
-            int random4 = (int)(Math.random()*4);
-            switch (random4) {
-                case "THH"; //7:1 3:1
-                case "HHT"; //2:1 2:1
-                case "TTH"; //2:1 2:1
-                case "HTT"; //7:1 3:1
-            }
-        } if (difficulty==2) {
-            String y = pChoice.charAt(1)
-            if (y.equals("H")) y = "T";
-            else y = "H";
-            return y + pChoice.charAt(0)+pChoice.charAt(1);
         }
+        if (pChoice.charAt(1)=='H')
+            return "T" + pChoice.charAt(0)+pChoice.charAt(1);
+        else
+            return "H" + pChoice.charAt(0)+pChoice.charAt(1);
     }
     /**
      * @return player's choice in HTH format if != cpuChoice
@@ -126,13 +123,20 @@ public class PenneyGame {
         while (true) {
             String INPUT = input.nextLine().toUpperCase();
             switch (INPUT) {
-                case "HHH": case "HHT": case "HTH": case "HTT":
-                case "THH": case "THT": case "TTH": case "TTT":
-                return INPUT;
-            } System.out.println("I didn't understand that. Try saying HHH");
+                case "HHH": case "HHT": case "HTH": case "HTT": case "THH": case "THT": case "TTH": case "TTT":
+                    if (!INPUT.equals(cpuChoice))
+                        return INPUT;
+                    //Else
+                    System.out.println("Hey I already chose "+cpuChoice+". Try something else");
+                    break;
+                default:
+                    System.out.println("I didn't understand that. Try saying HHH");
+            }
         }
     }
-    public static void player(String pChoice,String cpuChoice) {
-        //Call coin flip over and over until pChoice or cpuChoice happens
+    public static void play(String pChoice,String cpuChoice) {
+        while (true) {
+            
+        }
     }
 }
